71d1f98a191ea98ab821b1d4a673cd5b9968d94e
Olivier Katombe <olivierkatombe11@gmail.com>
2026-02-24 22:39:43 -0800

    test13

--- my-kinrtic-makeart/src/App.jsx
+++ my-kinrtic-makeart/src/App.jsx
@@ -11,8 +11,8 @@ function App() {
       <h1 className="center">MAKEART</h1>
       <div className="center">
         <p className="center">
-          Welcome to "MAKART" playground. A place you can enjoy learning and
-          making art yourself
+          Welcome to <p style="color:red;">"MAKART" </p>playground. A place you
+          can enjoy learning and making art yourself
         </p>
       </div>
       <p className="read-the-docs center">Click on The menu to learn more</p>
